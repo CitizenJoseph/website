fae4d1bfac9d53232d3854adadeb10b7b9fabb00
Joseph Cittadino <jcittadino01@gmail.com>
2021-02-14 22:08:51 -0500

    Fixed buttons and added flavicon

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
-let button = document.getElementById("trad");
-
-let changeCSS = () => {
-    document.getElementById("css").setAttribute("href", "./oldstyle.css");
-    document.getElementById("trad").setAttribute("href", "./oldstyle.css");
-}
-
-button.onclick = changeCSS;
-
-if (document.getElementById("css").getAttribute("href") === "./oldstyle.css") {
-    button.innerHTML = "Return to Modernity";
-}
+if (document.getElementById("button-link").getAttribute("href") === "./index.html")
+    document.getElementById("button-link").innerHTML = "Return to Modernity";
